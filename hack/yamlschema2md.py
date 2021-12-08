49e04371e861fc16835a657af711838babcbc790
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2021-12-08 13:18:53 +0530

    add: pipenv fix (temporary) (#63)
    fix: (temorary) remove `pipenv lock --pre` to fix dependency issues
    
    Signed-off-by: Yashvardhan Kukreja <ykukreja@redhat.com>

--- hack/yamlschema2md.py
+++ hack/yamlschema2md.py
@@ -5,7 +5,9 @@ import yaml
 
 
 def main(schema, output):
-    with open(schema, "r") as yaml_in, open(output, "w") as md_out:
+    with open(schema, "r", encoding="utf8") as yaml_in, open(
+        output, "w", encoding="utf8"
+    ) as md_out:
         yaml_obj = yaml.load(yaml_in, Loader=yaml.CSafeLoader)
         md_lines = jsonschema2md.Parser().parse_schema(yaml_obj)
         md_out.write("\n".join(md_lines))
--- tests/sss/test_pagerduty.py
+++ tests/sss/test_pagerduty.py
@@ -1,8 +1,6 @@
 import pytest
 
-from tests.testutils.addon_helpers import (
-    addon_with_pagerduty,
-)  # noqa: F401
+from tests.testutils.addon_helpers import addon_with_pagerduty  # noqa: F401
 
 
 @pytest.mark.parametrize(
@@ -18,22 +16,24 @@ def test_pagerduty(addon_str, request):
             "name": f"addon-{addon.metadata['id']}",
         },
         "spec": {
-            "servicePrefix": addon.metadata['id'],
-            "acknowledgeTimeout": addon.metadata['pagerduty']['acknowledgeTimeout'],
-            "resolveTimeout": addon.metadata['pagerduty']['acknowledgeTimeout'],
-            "escalationPolicy": addon.metadata['pagerduty']['escalationPolicy'],
+            "servicePrefix": addon.metadata["id"],
+            "acknowledgeTimeout": addon.metadata["pagerduty"][
+                "acknowledgeTimeout"
+            ],
+            "resolveTimeout": addon.metadata["pagerduty"]["acknowledgeTimeout"],
+            "escalationPolicy": addon.metadata["pagerduty"]["escalationPolicy"],
             "clusterDeploymentSelector": {
                 "matchLabels": {
                     f"api.openshift.com/addon-{addon.metadata['id']}": "true"
                 }
             },
             "targetSecretRef": {
-                "name": addon.metadata['pagerduty']['secretName'],
-                "namespace": addon.metadata['pagerduty']['secretNamespace'],
+                "name": addon.metadata["pagerduty"]["secretName"],
+                "namespace": addon.metadata["pagerduty"]["secretNamespace"],
             },
         },
     }
-    
+
     sss_walker = addon.sss.walker()
     pagerduty_obj = sss_walker["pdi"]
     assert pagerduty_obj is not None
@@ -43,8 +43,19 @@ def test_pagerduty(addon_str, request):
     metadata, spec = pagerduty_obj.get("metadata"), pagerduty_obj.get("spec")
     assert metadata.get("name") == "addon-test-operator"
     assert spec.get("servicePrefix") == expected_data["spec"]["servicePrefix"]
-    assert spec.get("acknowledgeTimeout") == expected_data["spec"]["acknowledgeTimeout"]
+    assert (
+        spec.get("acknowledgeTimeout")
+        == expected_data["spec"]["acknowledgeTimeout"]
+    )
     assert spec.get("resolveTimeout") == expected_data["spec"]["resolveTimeout"]
-    assert spec.get("escalationPolicy") == expected_data["spec"]["escalationPolicy"]
-    assert spec.get("clusterDeploymentSelector") == expected_data["spec"]["clusterDeploymentSelector"]
-    assert spec.get("targetSecretRef") == expected_data["spec"]["targetSecretRef"]
+    assert (
+        spec.get("escalationPolicy")
+        == expected_data["spec"]["escalationPolicy"]
+    )
+    assert (
+        spec.get("clusterDeploymentSelector")
+        == expected_data["spec"]["clusterDeploymentSelector"]
+    )
+    assert (
+        spec.get("targetSecretRef") == expected_data["spec"]["targetSecretRef"]
+    )
