abeab7678114e750a95bdaa954041c5b5aac4285
ashish <asnaraya@redhat.com>
2021-12-22 13:30:34 +0530

    dind changes for dockerfile.test
    Signed-off-by: ashish <asnaraya@redhat.com>

--- pr_check.sh
+++ pr_check.sh
@@ -5,4 +5,11 @@ set -exvo pipefail -o nounset
 IMAGE_TEST=managedtenants-cli
 
 docker build -t ${IMAGE_TEST} -f Dockerfile.test .
-docker run --rm ${IMAGE_TEST} check test
+
+docker_run_args=(
+    --rm
+    -v "/var/run/docker.sock:/var/run/docker.sock"
+)
+
+
+docker run "${docker_run_args[@]}" "${IMAGE_TEST}" check test
