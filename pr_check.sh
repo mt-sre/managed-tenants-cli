dcf52be6ff925c8db0876ed3c927ac1e6d790cca
Andrew Pantuso <apantuso@redhat.com>
2024-06-27 08:40:05 -0400

    fix

--- pr_check.sh
+++ pr_check.sh
@@ -8,9 +8,8 @@ docker build -t ${IMAGE_TEST} -f Dockerfile.test .
 
 docker_run_args=(
     --rm
-    -v "/var/run/docker.sock:/var/run/docker.sock"
+    -v "/run/user/$(id -u)/docker.sock:/var/run/docker.sock"
     --net "host"
 )
-
 
 docker run "${docker_run_args[@]}" "${IMAGE_TEST}" check test
